ef14318af3aee0213169b1df7d9c799fd868237a
Hanzzz123 <aanzs496@gmail.com>
2024-11-09 17:32:57 +0700

    Add Landing Page

---
+++ wonders-landing-page/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import LandingPage from './components/LandingPage.js';
+
+function App() {
+  return (
+      <div>
+        <LandingPage />
+      </div>
+  );
+}
+
+export default App;
